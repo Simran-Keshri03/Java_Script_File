29e0d76ff05daa65c2d2c7a4ce126857d7d9a5c4
Simran Keshri <simrangrd373@gmail.com>
2026-05-12 22:13:47 +0530

    Update DataTypes.js

--- Java_Script/DataTypes.js
+++ Java_Script/DataTypes.js
@@ -4,18 +4,19 @@ c = "simran";
 d= true;
 e = 'q';
 
-let name = "Simran"; // String
-let age = 20; // Number
-let isStudent = true; // Boolean
-let marks = null; // Null
-let x; // Undefined
-
 
 console.log(a ,typeof (a));
 console.log(b ,typeof (b));
 console.log(c ,typeof (c));
 console.log(d ,typeof (d));
 console.log(e ,typeof (e));
+
+let name = "Simran"; // String
+let age = 20; // Number
+let isStudent = true; // Boolean
+let marks = null; // Null
+let x; // Undefined
+
 
 console.log(name);
 console.log(age);
